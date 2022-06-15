70e01482227392156a6c3205c8a913199a9b1382
Ankita Sharma <ankitalrm@gmail.com>
2022-06-15 12:47:08 +0530

    fix for UBuntu 18.04

--- AppImage_FLINT_Build.sh
+++ AppImage_FLINT_Build.sh
@@ -4,20 +4,40 @@
 # The AppImage built using this script has been, so far, tested on Ubuntu 22.04 derivatives.
 
 sudo apt -y update && sudo apt -y upgrade
-sudo apt -y install cmake build-essential
+sudo apt -y install build-essential
+sudo apt -y install libfuse2
 
 WORKDIR=/home/$USER/Downloads
 mkdir -p $WORKDIR
+
+# Remove existing versions of CMake (if any)
+sudo apt purge cmake
+
+# CMake needs OpenSSL (1.1.1o is the preferred versionas of June 2022)
+wget https://www.openssl.org/source/openssl-1.1.1o.tar.gz && \
+	tar -xvf openssl-1.1.1o.tar.gz && \
+	cd openssl-1.1.1o && \
+	./config && \
+	make && \
+	sudo make install
+
+# Install latest CMake version - sudo apt install cmake is outdated!!
+wget https://github.com/Kitware/CMake/releases/download/v3.24.0-rc1/cmake-3.24.0-rc1.tar.gz && \
+	tar -xvf cmake-3.24.0-rc1.tar.gz && \
+	cd cmake-3.24.0-rc1 && ./bootstrap && make && sudo make install
+
+# Install SQLite3
+sudo apt-get -y install libsqlite3-dev libsqlite3-0 sqlite3
 
 # Install Boost
 sudo apt-get -y install libboost-all-dev
 
 # Build Turtle from source
-cd $WORKDIR && git clone https://www.github.com/mat007/turtle.git && cd turtle && mkdir -p build && cd build && cmake .. && make -j$nproc && sudo make install
+cd $WORKDIR && git clone https://www.github.com/mat007/turtle.git && cd turtle && mkdir -p build && cd build && cmake .. && make && sudo make install
 
 # Install unixODBC
 cd $WORKDIR && wget http://www.unixodbc.org/unixODBC-2.3.11.tar.gz && gunzip unixODBC-2.3.11.tar.gz && tar xvf unixODBC*.tar
-cd $WORKDIR && cd unixODBC-2.3.11 && ./configure && make -j$nproc && sudo make install
+cd $WORKDIR && cd unixODBC-2.3.11 && ./configure && make && sudo make install
 
 # Install sqlite3
 sudo apt -y install libsqlite3-dev
@@ -48,12 +68,12 @@ cd $WORKDIR && cd poco-poco-1.11.1-release && mkdir -p cmake-build && cd cmake-b
         -DENABLE_SEVENZIP=OFF \
         -DENABLE_ZIP=OFF \
         -DENABLE_PAGECOMPILER=OFF \
-        -DENABLE_PAGECOMPILER_FILE2PAGE=OFF .. && make -j$nproc && sudo make install
+        -DENABLE_PAGECOMPILER_FILE2PAGE=OFF .. && make && sudo make install
 
 # This should be changed to moja-global/FLINT.git once this PR (https://github.com/moja-global/FLINT/pull/119)
 # is merged.
 cd $WORKDIR && git clone https://www.github.com/ankitaS11/FLINT.git && cd FLINT && git checkout fix_poco_include && cd Source && mkdir -p build && cd build && \
-	cmake .. && make -j$nproc && make install DESTDIR=AppDir
+	cmake .. && make && make install DESTDIR=AppDir
 
 # Install linuxdeploy
 cd $WORKDIR/FLINT/Source/build/ && wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage && chmod +x linuxdeploy-x86_64.AppImage
